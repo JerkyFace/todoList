6a8798264a07ecf784d4c9ac51f668d1769adbf2
Nike Bliznyuk <nike.bliznyuk@gmail.com>
2018-06-21 00:03:59 +0600

    todos.js: check off action, remove todo action, add new todo action

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,21 @@
-$('li').on('click', function () {
-    if ($(this).css('color') === "rgba(22, 22, 22, 0.7)") {
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        })
-    }
-    else {
-        $(this).css({
-            color: "rgba(22,22,22, 0.7)",
-            textDecoration: "line-through"
-        })
+//Check off a list item
+$('ul').on('click', 'li', function () {
+    $(this).toggleClass('li-clicked')
+})
+
+//
+$('ul').on('click', 'span', function (event) {
+    $(this).parent().fadeOut(400, function () {
+        $(this).remove()
+    })
+})
+
+//
+$('input[type="text"]').keypress(function (event) {
+    if (event.which === 13) {
+        let text = $(this).val()
+        //add new li
+        $('ul').append(`<li><span>o</span> &nbsp; ${text} </li>`)
+        $(this).val("")
     }
 })
